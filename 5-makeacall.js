88860747c4df282cd99152103cbf16075ccd5597
diggz <diggz@gwob.org>
2013-01-03 03:32:36 -0500

    fixes

--- 5-makeacall.js
+++ 5-makeacall.js
@@ -1,5 +1,5 @@
-call('+14077586356', {
-   callerID:"14075550100"});
+call("+14077586356", {
+   callerID:"14242817736"});
 wait(500);
 
-say("Hello.  This is a message from the Man.  Stop talking on the phone.  Thank you.");
+say("Hello.  This is a message from the Man.  Stop talking on the phone.  Thank you." , {voice:"Dave"});
