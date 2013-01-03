284e8d9a1112438cae66465f17b1ef38b299b1b4
diggz <diggz@gwob.org>
2013-01-03 03:05:18 -0500

    added + to phone number

--- 5-makeacall.js
+++ 5-makeacall.js
@@ -1,6 +1,6 @@
 call(['+14077586356']{
    timeout:120,
-   callerID:'14242817736',
+   callerID:'+14242817736',
       onAnswer: function() {
        say("Tag, you're it!");
        log("Obnoxious call complete");
