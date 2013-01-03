5052488574110692af200e7c985fcea18864a7c1
diggz <diggz@gwob.org>
2013-01-03 03:10:04 -0500

    simplified

--- 5-makeacall.js
+++ 5-makeacall.js
@@ -1,14 +1,4 @@
-call(['+14077586356']{
+call("+14077586356"{
    timeout:120,
-   callerID:'+14242817736',
-      onAnswer: function() {
-       say("Tag, you're it!");
-       log("Obnoxious call complete");
-   },
-   onTimeout: function() {
-       log("Call timed out");
-   },
-   onCallFailure: function() {
-       log("Call could not be completed as dialed");
-   }
-});
+   callerID:"+14242817736"});
+say("Tag, you’re it!");
